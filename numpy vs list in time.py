30d4e7edecc435f8355a76f6d28be938c2c4bd99
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-12-28 13:07:02 +0530

    Create numpy vs list in time.py

---
+++ numpy vs list in time.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.arange(1000000)
+pylist = list(range(1000000))
+
+%time for item in range(10): [item * 3 for item in pylist] # for list
+%time for item in range(10): arr = arr * 3 # for numpy
