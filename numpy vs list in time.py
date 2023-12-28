d2fbfada2e1eb3aa796def6d6e6e341796f2e0f0
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-12-28 13:07:45 +0530

    Update numpy vs list in time.py

--- numpy vs list in time.py
+++ numpy vs list in time.py
@@ -5,3 +5,5 @@ pylist = list(range(1000000))
 
 %time for item in range(10): [item * 3 for item in pylist] # for list
 %time for item in range(10): arr = arr * 3 # for numpy
+
+# numpy wall time is less than list wall time
